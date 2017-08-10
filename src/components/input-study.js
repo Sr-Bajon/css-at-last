e0a704790b5c5f58cf9b6e2a52c8777f648febd7
Ismael_R <ismael.rodriguez@atmira.com>
2017-08-10 14:31:12 +0200

    mas ejemplos de input

--- src/components/input-study.js
+++ src/components/input-study.js
@@ -1,40 +1,65 @@
 import React from 'react'
 import { StyleSheet, css } from 'aphrodite/no-important'
 
-const InputStudy = () => {
+const InputGlow = () => {
   return (
-    <div>
-      <input
-        className={css(styles.glow)}
-        type='text'
-        placeholder='placeholder'
-        required
-      />
-      <input
-        className={css(styles.thick)}
-        type='text'
-        placeholder='placeholder'
-        required
-      />
-      <input
-        className={css(styles.double)}
-        type='text'
-        placeholder='placeholder'
-        required
-      />
-      <input
-        className={css(styles.underline)}
-        type='text'
-        placeholder='placeholder'
-        required
-      />
-      <input
-        className={css(styles.inset)}
-        type='text'
-        placeholder='placeholder'
-        required
-      />
-    </div>
+    <input
+      className={css(styles.glow)}
+      type='text'
+      placeholder='glow'
+      required
+    />
+  )
+}
+const InputThick = () => {
+  return (
+    <input
+      className={css(styles.thick)}
+      type='text'
+      placeholder='thick'
+      required
+    />
+  )
+}
+const InputDouble = () => {
+  return (
+    <input
+      className={css(styles.double)}
+      type='text'
+      placeholder='double'
+      required
+    />
+  )
+}
+const InputUnderline = () => {
+  return (
+
+    <input
+      className={css(styles.underline)}
+      type='text'
+      placeholder='underline'
+      required
+    />
+  )
+}
+const InputInset = () => {
+  return (
+    <input
+      className={css(styles.inset)}
+      type='text'
+      placeholder='inset'
+      required
+    />
+  )
+}
+const PlaceHolder = () => {
+  return (
+    <input
+      className={css(styles.placeholder)}
+      type='text'
+      placeholder='placeholder'
+      required
+    />
   )
 }
 
@@ -84,7 +109,27 @@ const styles = StyleSheet.create({
     ':focus': {
       'box-shadow': 'inset 1px 1px 2px 0 #c9c9c9'
     }
+  },
+  placeholder: {
+    // espacio entre el borde y las letras
+    padding: '20px',
+    border: 'solid 1px #dcdcdc',
+    // se anima el box shadow y el color del borde al hacer focus
+    transition: 'box-shadow 0.3s, border 0.3s',
+    ':focus': {
+      border: 'solid 1px #707070',
+      'box-shadow': '0 0 5px 1px #969696'
+    },
+    // afecta solo al texto del placeholder
+    '::placeholder': {
+      color: 'pink'
+    },
+    // afecta al elemento mientras se esté mostrando el placeholder
+    ':placeholder-shown': {
+      border: 'solid 1px #D82614'
+    }
   }
 })
 
-export default InputStudy
+export default [InputGlow, InputThick, InputDouble, InputUnderline, InputInset,
+  PlaceHolder]
--- src/index.js
+++ src/index.js
@@ -3,21 +3,31 @@ import ReactDOM from 'react-dom'
 import './style/main.css'
 import { StyleSheet, css } from 'aphrodite/no-important'
 
-import InputStudy from './components/input-study'
+import InputsStudy from './components/input-study'
+
+console.log(InputsStudy)
 
 let root = document.createElement('div')
 root.setAttribute('id', 'root')
 document.body.appendChild(root)
 
 const HolaComponent = () => (
-  <div id='grid-container' className={css(styles.container)}>
+  <div id='grid-container' className={css(styles.container)} >
     <header className={css([styles['item-a'], styles.backGrey])} />
-    <div className={css(styles['item-c'])}><p>Input type="text"</p></div>
-    <div className={css([styles['item-b']])}>
-      <InputStudy />
-    </div>
+    <div className={css(styles['item-c'])} ><p >Input type="text"</p ></div >
+    <div className={css([styles['item-b'], styles.inputFlexboxContainer])} >
+      {InputsStudy.map((MyInput, index, arr) => {
+        return (
+          <div
+            className={++index !== arr.length
+              ? css(styles.espacioTop)
+              : css([styles.espacioTop, styles.espacioDown])} >
+            <MyInput />
+          </div >)
+      })}
+    </div >
     <footer className={css([styles['item-d'], styles.backGrey])} />
-  </div>
+  </div >
 )
 
 const styles = StyleSheet.create({
@@ -52,11 +62,27 @@ const styles = StyleSheet.create({
     'grid-area': 'footer'
   },
 
+  espacioTop: {
+    // margin para que quede espacio entre los inputs
+    marginTop: '1em'
+  },
+
+  espacioDown: {
+    // margin para que quede espacio entre los inputs
+    marginBottom: '1em'
+  },
+
+  inputFlexboxContainer: {
+    display: 'flex',
+    'flex-direction': 'column',
+    'justify-content': 'space-around'
+  },
+
   container: {
     display: 'grid',
     // 'grid-template-columns': '200px 200px 200px 200px',
-    'grid-template-columns': '20% 80%',
-    'grid-template-rows': '100px 100px 100px',
+    'grid-template-columns': '10% 90%',
+    'grid-template-rows': '100px auto 100px',
     'grid-template-areas': `"header header"
                            "text main"
                            "footer footer"`
